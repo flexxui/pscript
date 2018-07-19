fbcd3671c95307126f9906868963f8b6f13bb310
Almar Klein <almar.klein@gmail.com>
2018-07-19 14:12:58 +0200

    undo ast change for py3.7dev that did not make it in 3.7 final

--- pscript/commonast.py
+++ pscript/commonast.py
@@ -804,8 +804,6 @@ class NativeAstConverter:
         node = Module([])
         # Add back the "docstring" that Python removed; this may actually be
         # a code snippet and not a module.
-        if pyversion > (3, 7) and n.docstring:
-            node.body_nodes.append(Expr(Str(n.docstring)))
         self._stack.append((node.body_nodes, n.body))
         return node
     
@@ -1165,8 +1163,6 @@ class NativeAstConverter:
             for x in node.arg_nodes + node.kwarg_nodes:
                 assert isinstance(x, Arg)
         
-        if pyversion > (3, 7) and n.docstring:
-            node.body_nodes.append(Expr(Str(n.docstring)))
         self._stack.append((node.body_nodes, n.body))
         return node
     
@@ -1218,7 +1214,5 @@ class NativeAstConverter:
         node = ClassDef(n.name, [c(a) for a in n.decorator_list],
                         arg_nodes, kwarg_nodes, [])
         
-        if pyversion > (3, 7) and n.docstring:
-            node.body_nodes.append(Expr(Str(n.docstring)))
         self._stack.append((node.body_nodes, n.body))
         return node
